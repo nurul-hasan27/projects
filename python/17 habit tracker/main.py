f942c720260cd7538106742ebdf363c08de7a17f
nurul-hasan27 <149966361+nurul-hasan27@users.noreply.github.com>
2024-07-20 11:11:31 +0530

    Create main.py

---
+++ python/17 habit tracker/main.py
@@ -0,0 +1,54 @@
+import requests
+import datetime
+
+USERNAME = "nurulhasan"
+TOKEN = "x9b678bcv6568sz7c"
+today = datetime.datetime.now()
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_parameters = {
+    # token string is actually self created API-key
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "coding",
+    "unit": "hr",
+    "type": "float",
+    "color": "ajisai"
+}
+
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+# print(response.text)
+
+record_endpoint = f"{graph_endpoint}/{graph_config['id']}"
+formatted_date = today.strftime("%Y%m%d")
+record_config = {
+    "date": formatted_date,
+    "quantity": input("how many hours did you code today ? ")
+}
+
+response = requests.post(url=record_endpoint, json=record_config, headers=header)
+print(response.text)
+
+put_endpoint = f"{record_endpoint}/{formatted_date}"
+record_put = {
+    "quantity": "5"
+}
+
+# response = requests.put(url=put_endpoint, json=record_put, headers=header)
+# print(response.text)

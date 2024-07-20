873b4828c97df8290ac82ac043274fc92c329dbd
nurul-hasan27 <149966361+nurul-hasan27@users.noreply.github.com>
2024-07-20 11:22:50 +0530

    Create main.py

---
+++ python/20 spotify plalist/main.py
@@ -0,0 +1,9 @@
+import spotipy
+
+entered_date = input(f"which year would you want to travel? Type Date in this formate  YYY-MM-DD\n")
+print(entered_date)
+
+TOP_100_SONG_WEBSITE = f"https://www.billboard.com/charts/hot-100/{entered_date}/"
+
+
+# under process becausue it have some issue in verification of my account at spotify
